d7fed4f2c1dffbd09aa403741b79725bf07e48e6
zxl-fame <zxl2602647076@gmail.com>
2022-04-30 11:13:25 +0800

    first commit

---
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -0,0 +1,49 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A very simple (and not particularly useful)
+ * test class to have a starting point where to put tests.
+ *
+ * @author Arie van Deursen
+ */
+public class DirectionTest {
+    /**
+     * Do we get the correct delta when moving north?
+     */
+    @Test
+    void testNorth() {
+        Direction north = Direction.valueOf("NORTH");
+        
+        assertThat(north.getDeltaX()).isEqualTo(0);
+        assertThat(north.getDeltaY()).isEqualTo(-1);
+    }
+
+    @Test
+    void testSouth() {
+        Direction north = Direction.valueOf("SOUTH");
+
+        assertThat(north.getDeltaX()).isEqualTo(0);
+        assertThat(north.getDeltaY()).isEqualTo(1);
+    }
+
+    @Test
+    void testWest() {
+        Direction north = Direction.valueOf("WEST");
+
+        assertThat(north.getDeltaX()).isEqualTo(-1);
+        assertThat(north.getDeltaY()).isEqualTo(0);
+    }
+
+    @Test
+    void testEast() {
+        Direction north = Direction.valueOf("EAST");
+
+        assertThat(north.getDeltaX()).isEqualTo(1);
+        assertThat(north.getDeltaY()).isEqualTo(0);
+    }
+}
